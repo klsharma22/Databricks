9bbaf000e29f15a5efe894636277915ef10b218f
klsharma22 <klsharma22>
2024-07-16 22:14:59 +0000

    Adding New Notebooks

---
+++ MyFolder/(Clone) Demo Notebook.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+print("Hello World")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "Hello World from SQL";
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Title 1
+# MAGIC ## Title 2
+# MAGIC ### Title 3
+
+# COMMAND ----------
+
+
